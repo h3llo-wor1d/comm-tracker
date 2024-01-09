abaacbe51d5b9c1d9c83d3d9f5f052d51428366d
Wrench <contact.notashe@gmail.com>
2024-01-09 15:58:36 -0500

    add icons and stuff and things

--- src/page.js
+++ src/page.js
@@ -4,6 +4,7 @@ import BoardSectionList from "./components/BoardSectionList";
 export default function Page(props) {
     return (
         <div>
+            <img src="./header.png" alt="header" style={{position: "relative", left: "50%", transform: "translateX(-50%)", maxWidth: "75%", marginBottom: "20px"}} />
             <BoardSectionList />
             
         </div>
