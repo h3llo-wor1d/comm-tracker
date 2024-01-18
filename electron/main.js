9e49527051a19899e86042d98ee1f77e5fca89d9
willow-rubenstein <contact.notashe@gmail.com>
2024-01-18 15:32:03 -0500

    sync with master

--- electron/main.js
+++ electron/main.js
@@ -1,6 +1,9 @@
-const { app, BrowserWindow, screen: electronScreen } = require('electron');
+const { app, BrowserWindow, screen: electronScreen, session, Menu } = require('electron');
 const path = require('path');
+const { chrome } = require('process');
 const url = require('url');
+//const fetch = require("cross-fetch");
+//const { ElectronBlocker } = require('@cliqz/adblocker-electron');
 
 const createMainWindow = () => {
   let mainWindow = new BrowserWindow({
@@ -9,16 +12,53 @@ const createMainWindow = () => {
     show: false,
     backgroundColor: 'white',
     webPreferences: {
-      nodeIntegration: false
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false
     },
     icon: path.join(__dirname, '../icons/icon.png')
   });
+
+ 
+
+  /*ElectronBlocker.fromPrebuiltAdsAndTracking(fetch).then((blocker) => {
+    blocker.enableBlockingInSession(session.defaultSession);
+  });*/
 
   const startUrl = process.env.ELECTRON_START_URL || url.format({
     pathname: path.join(__dirname, '/../build/index.html'),
     protocol: 'file:',
     slashes: true
   });
+  
+  const extensionPopup = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  
+  extensionPopup.loadFile(path.join(__dirname, '../extensions/ublock/popup-fenix.html'));
+
+  const template = [
+    {
+      label: 'Extensions',
+      submenu: [
+        // Other menu items...
+        {
+          label: 'Open UBlock Origin',
+          click: () => {
+            // Add the functionality you want when the button is clicked
+            extensionPopup.show()
+          },
+        },
+      ],
+    },
+    // Other menu items...
+  ];
+
+  //const menu = Menu.buildFromTemplate(template);
+  //Menu.setApplicationMenu(menu);
 
   mainWindow.loadURL(startUrl);
 
@@ -30,7 +70,13 @@ const createMainWindow = () => {
   });
 };
 
-app.whenReady().then(() => {
+app.whenReady().then(async () => {
+  /*await session.defaultSession.loadExtension(
+    path.join(__dirname, '../extensions/ublock')
+  )*/
+
+  //console.log(`Loaded extensions: ${JSON.stringify(session.defaultSession.getAllExtensions(), null, 4)}`)
+
   createMainWindow();
 
   app.on('activate', () => {
